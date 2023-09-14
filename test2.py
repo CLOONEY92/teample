82beb0f5536db1b96847d5ee2afed12cf668a665
CLOONEY92 <kurashikijun@gmail.com>
2023-09-14 14:30:44 +0900

    test2도수정

--- test2.py
+++ test2.py
@@ -1,17 +1,16 @@
 # 평균을 구하는 함수
 
+def f_avg(data):
+    ans = sum(data) / len(data)
+    return(ans)
+    
+# 합계를 구하는 함수
+
 def f_sum(data):
     result = 0
     for i in data:
         result += i
     return result
-    
-# 합계를 구하는 함수
-
-def f_avg(data):
-    ans = sum(data) / len(data)
-    return(ans)
-
 
 # 오름차순으로 정렬해주는 함수
 def f_sort(data):
@@ -28,3 +27,4 @@ def f_sort(data):
 (f_sort([1,5,6,4]))
 
 print('뭐가 바뀌었는지 맞춰보시오!')
+print('다시 돌려놓았습니다!')

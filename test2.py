932b718e719a043e041f2f2c99770d6f10b5d4e9
kim Taeho <taeholeo@gmail.com>
2023-09-14 13:56:49 +0900

    김태호

--- test2.py
+++ test2.py
@@ -3,5 +3,15 @@
 # 합계를 구하는 함수
 
 # 오름차순으로 정렬해주는 함수
+def f_sort(data):
+    data.sort(reverse=False)
+    return(data)
 
+(f_sort([1,5,6,4]))
+   
 # 내림차순으로 정렬해주는 함수
+def f_sort(data):
+    data.sort(reverse=True)
+    return(data)
+
+(f_sort([1,5,6,4]))

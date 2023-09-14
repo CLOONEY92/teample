3ec27df51f029080a908e94706c0be7ed1e5f65a
CLOONEY92 <kurashikijun@gmail.com>
2023-09-14 13:54:04 +0900

    다시올렸어요

--- test2.py
+++ test2.py
@@ -6,6 +6,26 @@ def f_avg(data):
     
 # 합계를 구하는 함수
 
+def f_sum(data):
+    result = 0
+    for i in data:
+        result += i
+    return result
+
 # 오름차순으로 정렬해주는 함수
 
 # 내림차순으로 정렬해주는 함수
+
+def f_desc(data):
+    
+
+    for i in range(len(data)):
+        max_number = i
+
+        for j in range(len(data)):
+            if data[j] > data[max_number]:
+                max_number = j
+
+        data[i], data[max_number] = data[max_number], data[i]
+
+f_desc([1, 2, 3, 4])
